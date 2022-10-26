2098a1b9f11d926a7cac1d45f2a75ebfe2c7e890
Amal Abeygunawardana <amalabey@gmail.com>
2022-10-26 01:32:48 +0000

    Add secret scan support using gitleaks

--- .devcontainer/scripts/post-devcontainer-start.sh
+++ .devcontainer/scripts/post-devcontainer-start.sh
@@ -102,6 +102,10 @@ setup_sq_project $repo_name
 echo "Scripts: Make scripts executable"
 chmod +x /workspace/.devcontainer/scripts/post-commit.sh
 chmod +x /workspace/.devcontainer/scripts/run-code-scan.sh
+chmod +x /workspace/.devcontainer/scripts/run-secret-scan.sh
 
 echo "Git: Copying post-commit.sh as post-commit git hook"
 cp /workspace/.devcontainer/scripts/post-commit.sh /workspace/.git/hooks/post-commit
+
+echo "Git: Installing GitLeaks as a pre-commit hook"
+pre-commit install --allow-missing-config
